159f6fa52a63f921b78fb4e3536866c7b7637810
Manuel Salomon <manuel.salomondlc@gmail.com>
2021-05-21 12:50:12 +0200

    Moved variables to state and added refresh logic

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import AccountsTable from './components/AccountsTable/index.js';
 import TokensTable from './components/TokensTable/index.js';
 import Comptroller from './CompoundProtocol/Comptroller.js';
 import GasCosts from './CompoundProtocol/GasCosts.js';
+import OpenPriceFeed from './CompoundProtocol/OpenPriceFeed.js';
 import axios from 'axios';
 import Web3 from 'web3';
 
 const web3 = new Web3('http://192.168.1.2:8545');
 const troll = new web3.eth.Contract(Comptroller.abi, Comptroller.address);
+const priceFeed = new web3.eth.Contract(OpenPriceFeed.abi, OpenPriceFeed.address);
 
 
 function parsecTokenDataResponse(json, app) {
@@ -33,7 +35,7 @@ function parseAccountDataResponse(json, app) {
         symbol: token.symbol,
         supply: token.supply_balance_underlying.value,
         borrow: token.borrow_balance_underlying.value,
-        profit: token.supply_balance_underlying.value * app.closeFactor * (app.incentive - 1)
+        profit: token.supply_balance_underlying.value * app.state.closeFactor * (app.state.incentive - 1)
       }
     }
     );
@@ -79,40 +81,65 @@ function parseAccountDataResponse(json, app) {
 
 
 function Loader(props) {
-  props.app.refreshEthToUsd();
-  props.app.refreshCloseFactor();
-  props.app.refreshIncentive();
-  props.app.refreshGasPrice();
-  props.app.refreshTokenList();
-  props.app.refreshAccountsList();
+  props.app.refreshAccountList();
   return (<div />);
 }
 
 class App extends Component {
 
-  static ethToUsd;
-  static closeFactor;
-  static incentive;
-  static gasPrice;
-
   constructor() {
     super();
 
+    this.refreshEthToUsd = this.refreshEthToUsd.bind(this);
+    this.refreshCloseFactor = this.refreshCloseFactor.bind(this);
+    this.refreshIncentive = this.refreshIncentive.bind(this);
+    this.refreshGasPrice = this.refreshGasPrice.bind(this);
+    this.refreshAccountList = this.refreshAccountList.bind(this);
+    this.refreshTokenList = this.refreshTokenList.bind(this);
+
     this.state = {
+      ethToUsd: '',
+      closeFactor: '',
+      incentive: '',
+      gasPrice: '',
       accounts: [],
       cTokens: []
     };
   }
 
+  componentDidMount() {
+    this.refreshEthToUsd();
+    this.refreshCloseFactor();
+    this.refreshIncentive();
+    this.refreshGasPrice();
+    this.refreshTokenList();
+  }
+
   async refreshEthToUsd() {
-    this.ethToUsd = 3000;
+    //this.ethToUsd = 3000;
+
+    try {
+      let ethToUsd = await priceFeed.methods.getUnderlyingPrice('0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5').call() / 1e18;
+      console.log(ethToUsd);
+
+      this.setState({
+        ethToUsd: ethToUsd
+      })
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async refreshCloseFactor() {
     //this.closeFactor = 0.5;
 
     try {
-      this.closeFactor = await troll.methods.closeFactorMantissa().call() / 1e18;
+      let closeFactor = await troll.methods.closeFactorMantissa().call() / 1e18;
+      console.log(closeFactor);
+
+      this.setState({
+        closeFactor: closeFactor
+      })
     } catch (error) {
       console.error(error);
     }
@@ -123,7 +150,12 @@ class App extends Component {
     //this.incentive = 1.08;
 
     try {
-      this.incentive = await troll.methods.liquidationIncentiveMantissa().call() / 1e18;
+      let incentive = await troll.methods.liquidationIncentiveMantissa().call() / 1e18;
+      console.log(incentive);
+
+      this.setState({
+        incentive: incentive
+      })
     } catch (error) {
       console.error(error);
     }
@@ -134,14 +166,19 @@ class App extends Component {
     //this.gasPrice = 182e9;
 
     try {
-      this.gasPrice = await web3.eth.getGasPrice();
+      let gasPrice = await web3.eth.getGasPrice();
+      console.log(gasPrice);
+
+      this.setState({
+        gasPrice: gasPrice
+      })
     } catch (error) {
       console.error(error);
     }
 
   }
 
-  refreshTokenList() {
+  async refreshTokenList() {
     let URL = 'https://api.compound.finance/api/v2/ctoken';
 
     axios({
@@ -160,7 +197,7 @@ class App extends Component {
       });
   }
 
-  refreshAccountsList() {
+  async refreshAccountList() {
     let URL = 'https://api.compound.finance/api/v2/account';
 
     axios({
@@ -194,16 +231,22 @@ class App extends Component {
     }
 
     if (true) {
-      let liquidationFee = this.gasPrice * GasCosts.liquidateBorrow;
+      let liquidationFee = this.state.gasPrice * GasCosts.liquidateBorrow;
       return (
         <div className='App'>
+          <button style={{ float: 'right' }} onClick={this.refreshEthToUsd}> Refresh </button>
+          <h3> ETH - USD  </h3>
+          <span> {this.state.ethToUsd} USD </span>
+          <button style={{ float: 'right' }} onClick={this.refreshGasPrice}> Refresh </button>
+          <h3> Gas Price  </h3>
+          <span> {this.state.gasPrice} WEI </span>
           <h3> Liquidation Fee  </h3>
           <span> {liquidationFee} WEI, </span>
           <span> {liquidationFee / 1e9} GWEI, </span>
           <span> {liquidationFee / 1e18} ETH, </span>
-          <span> {(liquidationFee / 1e18) * this.ethToUsd} USD </span>
-          <button style={{ float: 'right' }} onClick={this.refreshAccountsList}> Refresh </button>
-          <AccountsTable accounts={this.state.accounts} ethToUsd={this.ethToUsd} />
+          <span> {(liquidationFee / 1e18) * this.state.ethToUsd} USD </span>
+          <button style={{ float: 'right' }} onClick={this.refreshAccountList}> Refresh </button>
+          <AccountsTable accounts={this.state.accounts} ethToUsd={this.state.ethToUsd} />
         </div>
       );
     }
