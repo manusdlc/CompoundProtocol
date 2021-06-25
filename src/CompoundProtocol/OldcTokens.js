85867c3a5d5243b98a80c58595978a1fc300ce4a
Manuel Salomon <manuel.salomondlc@gmail.com>
2021-06-25 12:45:56 +0200

    Added list of cTokens that cannot be liquidated when they are both borrowed and supplied

---
+++ src/CompoundProtocol/OldcTokens.js
@@ -0,0 +1,12 @@
+/*
+cETH, cUSDC, cZRX and cBAT can't be liquidated when both are supplied and borrowed. Work is being done at Compound to support this, but it is not ready yet.
+Corresponding addresses below
+*/
+
+const OldcTokens = [
+    "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5",
+    "0x39aa39c021dfbae8fac545936693ac917d5e7563",
+    "0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407",
+    "0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e"
+]
+export default OldcTokens;
